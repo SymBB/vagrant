53b77e18c0a8f1aabb37c655760ebccc2247703d
Christian Wielath <info@script-brain.de>
2014-10-08 10:16:51 +0200

    Update init.pp

--- puppet/modules/symbb/manifests/init.pp
+++ puppet/modules/symbb/manifests/init.pp
@@ -1,25 +1,32 @@
 class symbb {
 
-  require nginx
-  require utils
-  require php
-  require mysql
+    require nginx
+    require utils
+    require php
+    require mysql
 
-	$database_name = "symbb"
-	$database_user = "root"
-	$database_pw = "73wozGWmO1KgCBogtr8D"
-	$secret = "sdff342tvgdf3hb3f35s"
+    $database_name = "symbb"
+    $database_user = "root"
+    $database_pw = "73wozGWmO1KgCBogtr8D"
+    $secret = "sdff342tvgdf3hb3f35s"
 
-	file { '/var/www/symbb/':
+   file { '/var/www/symbb/':
     ensure => absent ,
     recurse => true,
     purge => true,
     force => true,
   } ->
-	exec { 'sudo git clone https://github.com/SymBB/symbb_sandbox.git /var/www/symbb/':
-		path => '/usr/bin',
-		cwd => '/var/www/'
-	} ->
+  exec { 'sudo git clone https://github.com/SymBB/symbb_sandbox.git /var/www/symbb/':
+    path => '/usr/bin',
+    cwd => '/var/www/'
+  } ->
+    file { 'set owner of symbb dir':
+    path => '/var/www/symbb/',
+    ensure => directory ,
+    owner  => "www-data",
+    recurse => true,
+    force => true
+  }  ->
   file { 'vagrant-nginx-symbb':
     path => '/etc/nginx/sites-available/symbb',
     ensure => present,
@@ -30,17 +37,17 @@ class symbb {
       ensure  => 'link',
       target  => '/etc/nginx/sites-available/symbb',
   } ->
-	file { 'vagrant-nginx-symbb-paramerters':
-		path => '/var/www/symbb/app/config/parameters.yml',
-		ensure => present,
-    replace => true,
-		content => template('symbb/parameters.yml.erb'),
-	} ->
-	file { '/var/www/symbb/app/logs/':
+  file { 'vagrant-nginx-symbb-paramerters':
+      path => '/var/www/symbb/app/config/parameters.yml',
+      ensure => present,
+      replace => true,
+      content => template('symbb/parameters.yml.erb'),
+  } ->
+    file { '/var/www/symbb/app/logs/':
     ensure => directory ,
     owner  => "www-data"
   } ->
-	file { '/var/www/symbb/app/cache/':
+  file { '/var/www/symbb/app/cache/':
     ensure => directory ,
     owner  => "www-data"
   } ->
@@ -56,12 +63,12 @@ class symbb {
     ensure => present,
     owner  => "www-data"
   } ->
-	exec { 'sudo ant':
-		path => '/usr/bin',
-		timeout => 0,
-		cwd => '/var/www/symbb/build/install/',
-	} ->
-	exec { 'sudo /etc/init.d/nginx restart':
-		path => '/usr/bin'
-	}
+  exec { 'sudo ant':
+    path => '/usr/bin',
+    timeout => 0,
+    cwd => '/var/www/symbb/build/install/',
+  } ->
+  exec { 'sudo /etc/init.d/nginx restart':
+    path => '/usr/bin'
+  }
 }
